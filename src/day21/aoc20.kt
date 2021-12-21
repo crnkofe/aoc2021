28c8daf5f0e671041722a5c5d5a34ebf53f8ad94
Simon Mihevc <simon.mihevc@gmail.com>
2021-12-21 17:21:22 +0100

    day 21 - part 2

--- src/day21/aoc20.kt
+++ src/day21/aoc20.kt
@@ -7,7 +7,101 @@ fun roll(previous : Int) : Int{
     return previous+1
 }
 
-fun play(rounds : Int, start1 : Int, start2 : Int) : Int {
+data class DiracState(val p1pos : Int, val p2pos : Int, val p1score : Int, val p2score : Int, val playerIndex : Int)
+
+fun generateRollScores(pos : Int) : List<Int> {
+    var poss = mutableListOf<Int>()
+    for (i in 1..3) {
+        for (j in 1..3) {
+            for (k in 1..3) {
+                poss.add((pos + i + j + k) % 10)
+            }
+        }
+    }
+    /*
+    for (i in 1..3) {
+        for (j in 1..3) {
+            poss.add((pos + i + j) % 10)
+        }
+    }
+     */
+
+    return poss
+}
+
+fun playDirac(rounds : Int, start1 : Int, start2 : Int, score : Int) : Long {
+
+    // DP technique
+    // idea count number of times player 1 wins at score 20 and player 2 wins if score == 20
+    // then go backwards -- count number of times player 2 wins at 19 (directly, or indirectly)
+    // a die roll wins directly if score is larger than 21 (after player 1 rolls or player 2 rolls)
+    // a die roll wins indirectly if if score is still less than 21
+
+    var player1WinsCount = mutableMapOf<Int, Long>()
+    var player2WinsCount = mutableMapOf<Int, Long>()
+
+    // this is played on a 0-9 board to make modulos easier
+    var player1Pos = start1 - 1
+    var player2Pos = start2 - 1
+
+    for (round in 1..2) {
+        for (roll1 in generateRollScores(player1Pos)) {
+            for (roll2 in generateRollScores(player2Pos)) {
+                val score1 = roll1 + 1
+                if (score1 > round) {
+                    player1WinsCount[round] = player1WinsCount.getOrDefault(round, 0) + 1
+                    continue
+                }
+                val score2 = roll2 + 1
+                if (score2 > round) {
+                    player2WinsCount[round] = player2WinsCount.getOrDefault(round, 0) + 12
+                    continue
+                }
+            }
+        }
+    }
+
+    println("$player1WinsCount, $player2WinsCount")
+    return 0
+}
+
+
+fun playDiracNaive(round : Int, score1 : Int, score2: Int, start1 : Int, start2 : Int, limit : Int, cache : MutableMap<DiracState, List<Long>>) : List<Long> {
+    // cache needs to be separated by player
+    val ds = DiracState(start1, start2, score1, score2, round % 2)
+    if (cache.containsKey(ds)) {
+        return cache[ds] ?: listOf(0, 0)
+    }
+
+    var player1Turn = (round % 2) == 1
+    // not ideal but we check if current player is winner
+    if (!player1Turn && (score1 >= limit)) {
+        return listOf(1, 0)
+    }
+    if (player1Turn && (score2 >= limit)) {
+        return listOf(0, 1)
+    }
+
+    var player1WinsCount: Long = 0
+    var player2WinsCount: Long = 0
+
+    for (roll in generateRollScores(if (player1Turn) start1 else start2)) {
+        val newScore = (roll + 1)
+        val newScore1 = score1 + if (player1Turn) newScore else 0
+        val newScore2 = score2 + if (player1Turn) 0 else newScore
+        val newStart1 = if (player1Turn) roll else start1
+        val newStart2 = if (player1Turn) start2 else roll
+
+        val scores = playDiracNaive(round+1, newScore1, newScore2, newStart1, newStart2, limit, cache)
+        player1WinsCount += scores[0]
+        player2WinsCount += scores[1]
+    }
+
+    cache[ds] = listOf(player1WinsCount, player2WinsCount)
+    return listOf(player1WinsCount, player2WinsCount)
+}
+
+fun play(rounds : Int, start1 : Int, start2 : Int, scoreLimit : Int) : Int {
     var deterministicDie = 0
     // this is played on a 0-9 board to make modulos easier
     var player1Pos = start1 - 1
@@ -25,7 +119,7 @@ fun play(rounds : Int, start1 : Int, start2 : Int) : Int {
 
         countRolls += 3
 
-        if (score1 >= 1000) {
+        if (score1 >= scoreLimit) {
             return score2 * countRolls
         }
 
@@ -38,7 +132,7 @@ fun play(rounds : Int, start1 : Int, start2 : Int) : Int {
 
         countRolls += 3
 
-        if (score2 >= 1000 ) {
+        if (score2 >= scoreLimit ) {
             return score1 * countRolls
         }
 
@@ -48,14 +142,16 @@ fun play(rounds : Int, start1 : Int, start2 : Int) : Int {
 }
 
 
+
+
 fun main(args: Array<String>) {
     var fileName: String = args[0]
     try {
         val data = loadFileAsString(fileName).split("\n").map { it -> it.last().digitToInt() }
-
-        val part1Result = play(1000, data[0], data[1])
+        val part1Result = play(1000, data[0], data[1], 1000)
         println("Day 21 part 1 result: $part1Result")
-        val part2Result = 0
+        var cache = mutableMapOf<DiracState, List<Long>>()
+        val part2Result = playDiracNaive(1, 0, 0, data[0]-1, data[1]-1, 21, cache).maxOf { it }
         println("Day 21 part 2 result: $part2Result")
     } catch (e: FileNotFoundException) {
         println("Input file does not exist ($fileName)")
--- src/day21/aoc21_test.kt
+++ src/day21/aoc21_test.kt
@@ -8,6 +8,16 @@ import kotlin.test.assertEquals
 class Day21Tests {
     @Test
     fun shouldPlaySampleGame() {
-        assertEquals(739785, play(200, 4, 8))
+        assertEquals(739785, play(200, 4, 8, 1000))
+    }
+
+    @Test
+    fun shouldPlaySampleGamePart2() {
+        var cache = mutableMapOf<DiracState, List<Long>>()
+        // 4, 8
+        val result = playDiracNaive(1, 0, 0, 3, 7, 21, cache)
+        assertEquals(2, result.size)
+        assertEquals(444356092776315, result[0])
+        assertEquals(341960390180808, result[1])
     }
 }
